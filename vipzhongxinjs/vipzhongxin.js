7ec1e539321bb6ead4d397da2f4ae9980a8c7a05
至尊浅 <2452503702@qq.com>
2016-05-03 18:32:21 +0800

    1

---
+++ vipzhongxinjs/vipzhongxin.js
@@ -0,0 +1,28 @@
+$(function(){
+	$(".kaitongbox .kaitong").click(function(){
+		alert("已开通");
+		$(this).css({background:"#c1c1c1"});
+	})
+	// var a=location.href;
+	// $(".fuzhibtn").click(function(){
+	// 	 try{
+ //     // var rng = document.body.createTextRange();
+ //     var rng = location.href;
+ //     rng.moveToElementText(obj);
+ //     rng.scrollIntoView();
+ //     rng.select();
+ //     rng.execCommand("Copy");
+ //     rng.collapse(false);
+ //     alert("已经复制到粘贴板!你可以使用Ctrl+V 贴到需要的地方去了哦!");
+	// 	 }catch(e){
+	// 	     alert("您的浏览器不支持此复制功能，请选中相应内容并使用Ctrl+C进行复制!");
+	// 	 }
+
+	// })
+	// console.log(a);
+
+
+
+
+
+})
